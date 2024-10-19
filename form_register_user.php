1343e44e0c6f657e1fb1be99811ed8c94f9e23da
Nagdl <137997774+Nagdl@users.noreply.github.com>
2024-10-20 00:53:53 +0700

    Add CSS for Registration

--- form_register_user.php
+++ form_register_user.php
@@ -1,10 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Register</title>
-</head>
-<body>
-
+<!-- PHP -->
 <?php
 $firstnameErr = $lastnameErr = $usernameErr = $emailErr = $passwordErr = $confirmPasswordErr = "";
 $firstname = $lastname = $username = $email = $password = $confirmPassword = "";
@@ -24,6 +18,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($password !== $confirmPassword) {
         $confirmPasswordErr = "Passwords do not match";
         $valid = false;
+        echo "<script>alert('$confirmPasswordErr');</script>";
     }
 
     if ($valid) {
@@ -38,9 +33,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         mysqli_stmt_bind_param($stmt, "sssss", $firstname, $lastname, $username, $email, $hashed_password);
 
         if (mysqli_stmt_execute($stmt)) {
-            echo "Registration successful!";
+            echo "<script>alert('Registration successful!');</script>";
         } else {
-            echo "Something went wrong. Please try again later.";
+            echo "<script>alert('Something went wrong. Please try again later.');</script>";
         }
 
         // Close the statement and connection
@@ -57,34 +52,117 @@ function test_input($data) {
 }
 ?>
 
-<h2>Register</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    First Name: <input type="text" name="firstname" required value="<?php echo $firstname;?>">
-    <span><?php echo $firstnameErr;?></span>
-    <br><br>
+<!-- HTML -->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Register</title>
+    <link rel="stylesheet" href="user_regis.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <!-- Overlay -->
+    <div class="overlay"></div>
 
-    Last Name: <input type="text" name="lastname" required value="<?php echo $lastname;?>">
-    <span><?php echo $lastnameErr;?></span>
-    <br><br>
+    <!-- Container -->
+    <div class="container">
+        
+        <!-- Bagian Kiri -->
+        <div class="Kiri">
+            <div class="bungkusKiri">                
+                <a href="form_login_user.php" class="tabLogin">
+                    Sign In
+                </a>
+                <p class="tema fontPutih">
+                    Event Organizer
+                </p>
+                <p class="logo">
+                    <span class="fontCokelat">O</span><span class="fontPutih">VENT</span>
+                </p>
+                <p class="moto fontPutih">
+                    Your favorite event organizer @Ovent
+                </p>
+            </div>
+        </div>
 
-    Username: <input type="text" name="username" required value="<?php echo $username;?>">
-    <span><?php echo $usernameErr;?></span>
-    <br><br>
+        <!-- Bagian Kanan -->
+        <div class="Kanan">
+            <div class="bungkusKanan">
+                <div class="titleRegis">
+                    <p class="titleForm">
+                        <span class="fontHitam">Create New<br>Account</span><span class="fontCokelat">.</span>
+                    </p>
+                    <p class="descForm">
+                        <span class="fontHitam">Start your journey with OVENT</span>
+                    </p>
+                </div>
 
-    Email: <input type="email" name="email" required value="<?php echo $email;?>">
-    <span><?php echo $emailErr;?></span>
-    <br><br>
+                <!-- Formulir -->
+                <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                    <!-- Name -->
+                    <div class="regisName list-data">
+                        <div>
+                            <span class="fontHitam Font_Size">First Name<br></span>
+                            <div class="group">                                
+                                <input class="input-nama" type="text" name="firstname" required value="<?php echo $firstname;?>">
+                                <span><?php echo $firstnameErr;?></span>
+                            </div>
+                        </div>   
+                        <div>
+                            <span class="fontHitam Font_Size">Last Name<br></span>
+                            <div class="group">                                
+                                <input class="input-nama" type="text" name="lastname" required value="<?php echo $lastname;?>">
+                                <span><?php echo $lastnameErr;?></span>
+                            </div>
+                        </div>
+                    </div>
 
-    Password: <input type="password" name="password" required>
-    <span><?php echo $passwordErr;?></span>
-    <br><br>
+                    <!-- UserName -->
+                    <div class="list-data">       
+                        <span class="fontHitam Font_Size">Username<br></span>
+                        <div class="group">
+                            <i class="fa-solid fa-user icon"></i>
+                            <input class="input-data" type="text" name="username" required value="<?php echo $username;?>">
+                            <span><?php echo $usernameErr;?></span> 
+                        </div>
+                    </div>
 
-    Confirm Password: <input type="password" name="confirm_password" required>
-    <span><?php echo $confirmPasswordErr;?></span>
-    <br><br>
+                    <!-- Email -->
+                    <div class="list-data">
+                        <span class="fontHitam Font_Size">Email<br></span>
+                        <div class="group">   
+                            <i class="fa-regular fa-envelope icon"></i>                             
+                            <input class="input-data" type="email" name="email" required value="<?php echo $email;?>">
+                            <span><?php echo $emailErr;?></span>
+                        </div>
+                    </div>
 
-    <input type="submit" value="Register">
-</form>
-<a href="form_login_user.php">Login As User</a>
+                    <!-- Password + Confirm -->
+                    <div class="list-data">
+                        <span class="fontHitam Font_Size">Password<br></span>
+                        <div class="group">
+                            <i class="fa-solid fa-lock icon"></i>
+                            <input class="input-data" type="password" name="password" required>
+                            <span><?php echo $passwordErr;?></span>
+                        </div>
+                        
+                    </div>
+                    <div class="list-data">
+                        <span class="fontHitam Font_Size">Confirm Password<br></span>
+                        <div class="group">    
+                            <i class="fa-solid fa-lock icon"></i>                        
+                            <input class="input-data" type="password" name="confirm_password" required>
+                        </div>                       
+                    </div>
+                    <input class="buttonRegis" type="submit" value="Create Account">
+                </form>
+                <p class="info Font_Size">
+                    <span>Alredy have an account? </span><span><a href="form_login_user.php">Sign In</a></span>
+                </p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
